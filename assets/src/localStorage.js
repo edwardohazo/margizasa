625cc171a635b69d76f690806ce33742c8a8ec9f
wago <newguyonthemarket@gmail.com>
2025-02-28 15:22:16 -0600

    deleted folders

--- assets/src/localStorage.js
+++
@@ -1,9 +0,0 @@
-export const setCurrentTrip = (trip) => {
-    console.log(trip);
-    localStorage.setItem('current-trip', trip)
-};
-export const getCurrentTrip = () => {
-    const currentTrip = localStorage.getItem('current-trip')
-    return JSON.parse(currentTrip);
-};
-
--- assets/src/utils/config.js
+++
@@ -1,2 +0,0 @@
-// export const apiUrl = 'http://localhost:5000';
-export const apiUrl = 'https://ocean-experience-rest-api.onrender.com';
